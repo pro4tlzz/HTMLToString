82438569a037476b68f2dcfce5a0cb0e41057af5
Bilal Habib <superfrillz12@hotmail.co.uk>
2018-06-28 18:43:03 +0100

    Cleaned code and readability

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -9,25 +9,23 @@ namespace HTMLToString
         static void Main(string[] args)
         {
 
-
-            Console.WriteLine("Enter a Valid Webpage URL such as http://wwww.google.com, this tool will download the HTML into a .txt file");
+            
+            Console.WriteLine("Enter a Valid Webpage URL such as http://wwww.google.com," +
+                " this tool will download the HTML into a .txt file");
             string webPage = Console.ReadLine();
-            Uri uriResult;
-            bool result = Uri.TryCreate(webPage, UriKind.Absolute, out uriResult)
-                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
             WebClient client = new WebClient();
             //DownloadsWebpage
-            string reply = client.DownloadString(webPage);
-
+             string reply = client.DownloadString(webPage);
             Console.WriteLine(reply);
             //Location of file here
-            Console.WriteLine("Please enter a valid address to save the .txt file such as C:\\Users\\Public\\String.txt, then press any button to exit");
+            Console.WriteLine("Please enter a valid address to save the .txt file such as C:\\Users\\Public\\String.txt," +
+                " then press any button to exit");
             string txtDir = Console.ReadLine();
             File.WriteAllText(txtDir, reply);
             Console.ReadLine();
             //Checks if file exists
-            Console.WriteLine(File.Exists(txtDir) ? "File exists, please press enter to exit." : "File does not exist, please press enter to exit.");
-      
+            Console.WriteLine(File.Exists(txtDir) ? "File exists, please press enter to exit."
+                : "File does not exist, please press enter to exit.");
             Console.ReadLine();
             Environment.Exit(0);
         }
