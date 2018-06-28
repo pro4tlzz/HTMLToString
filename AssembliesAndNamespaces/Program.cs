ec48625533c3d509f8a44f45bff9e3a85e3e7963
Bilal Habib <superfrillz12@hotmail.co.uk>
2018-06-28 16:22:39 +0100

    Added user input

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace HTMLToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+          
+            File.WriteAllText(@"D:\WriteText.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
